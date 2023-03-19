e0e3bda38ea384eafb825c134a739bc5733e94e1
Sanchez-Gabriel <DonSanchezGabriel@gmail.com>
2023-03-19 19:26:38 -0400

    Imported dbs HTML mockup into Node.js and Express

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,8 +1,9 @@
-const { restart } = require("nodemon")
+var fs = require('fs');
 
+var  trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 /*GET travel view */
-const travel=(req, res)=> {
-        res.render('travel',{title:'Travlr Getawayss'});
+const travel = (req, res) => {
+    res.render('travel', {title: "Travlr Getaways - Travel", trips});
 };
 module.exports = {
     travel
